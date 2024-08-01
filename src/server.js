5e73bfe6acd658a3f6de2a0bf4886386396854b4
Pedro Higor <pedro.higor92@gmail.com>
2024-07-31 22:16:25 -0300

    server and import type mode

--- src/server.js
+++ src/server.js
@@ -1,2 +1,23 @@
 
-echo ('hello word');
+import express from "express"
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res)=>{
+    res.send('Hello World')
+})
+
+app.get('/user', (req, res) => {
+    res.json({
+        nome: "Pedro",
+        email: "Pedro.Higor92@gmail.com",
+        religiao: "Rock",
+        Sexo: "Masculino"
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`Servidor rodando no http://localhost:3000`)
+})
